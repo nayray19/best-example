b06a2d3acde97c5bf6564e3a708eb3efedd3fe2a
nayray19 <nayray@gmail.com>
2024-06-26 20:49:04 -0400

    :art
    Fixes issue

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-“document.addEventListener(         
+document.addEventListener(         
     "DOMContentLoaded",         
     function(event) {         
-        alert('The page is loaded and the script ran!')         
-});”
+        var message = document.getElementById('message')
+        message.innerText = 'The script ran!'         
+    }
+
+);
